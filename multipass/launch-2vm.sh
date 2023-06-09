26e099a413c37708d0f320d8e674c73556f84735
zhouxh <robertzhouxh@gmail.com>
2023-06-09 19:28:33 +0800

    reduce resource

--- multipass/launch-2vm.sh
+++ multipass/launch-2vm.sh
@@ -4,7 +4,7 @@
 ## en0 表示要桥接的网卡
 # multipass launch --name o-worker -c 2 -m 2G jammy --disk 30G --cloud-init systemd-resolved.yaml--network en0
 
-multipass launch --name master -c 4 -m 8G jammy --disk 60G --cloud-init systemd-resolved.yaml
+multipass launch --name master -c 4 -m 6G jammy --disk 60G --cloud-init systemd-resolved.yaml
 multipass launch --name e-worker -c 2 -m 2G jammy --disk 30G --cloud-init systemd-resolved.yaml
 multipass launch --name e-node -c 2 -m 2G jammy --disk 30G --cloud-init systemd-resolved.yaml
 
